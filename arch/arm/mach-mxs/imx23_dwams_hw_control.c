2fd49a0cda867ae7fbd168dd60e084a5080d218d
Jari Tervonen <jari.tervonen@navicron.com>
2013-12-19 11:18:05 +0200

    Add DWAMS HW3 fixes.
    Change-Id: I9bdcdae97238fb614f8a396fde0f1a5597ef5d42
    
    Signed-off-by: Jari Tervonen <jari.tervonen@navicron.com>

--- arch/arm/mach-mxs/imx23_dwams_hw_control.c
+++ arch/arm/mach-mxs/imx23_dwams_hw_control.c
@@ -51,20 +51,25 @@
 /* BT */
 #define RESET_BT       MXS_GPIO_NR(1, 20) //MXS_PIN_TO_GPIO(PINID_LCD_WR)
 #define VBUS_EN            MXS_GPIO_NR(1, 15) //MXS_PIN_TO_GPIO(PINID_LCD_D15)
+#define EN_VBUS_CSR		MXS_GPIO_NR(1, 16) //MXS_PIN_TO_GPIO(PINID_LCD_D16)
+#define EN_VBUS_IMX		MXS_GPIO_NR(1, 17) //MXS_PIN_TO_GPIO(PINID_LCD_D17)
+/* Currently not used */ 
+#define MX_CSR_1		MXS_GPIO_NR(1, 24) //MXS_PIN_TO_GPIO(PINID_LCD_HSYNC)
+#define MX_CSR_2		MXS_GPIO_NR(1, 25) //MXS_PIN_TO_GPIO(PINID_LCD_VSYNC)
+
 /* AUDIO */
 #define HS_DETECT      MXS_GPIO_NR(1, 18) //MXS_PIN_TO_GPIO(PINID_LCD_RESET)
 #define MIC_GPIO_EN        MXS_GPIO_NR(1, 12) //MXS_PIN_TO_GPIO(PINID_LCD_D12)
 
-
+/* Debug GPIOS, can be used any debug purposes, e.g. for time measurement... */
+#define DEBUG_TP17		MXS_GPIO_NR(0, 6)  //MXS_PIN_TO_GPIO(PINID_GPMI_D06)
+#define DEBUG_TP20		MXS_GPIO_NR(0, 7)  //MXS_PIN_TO_GPIO(PINID_GPMI_D07)
 
 #if CONFIG_WL_TI
 void imx23_dwams_wlan_on_off(bool enable)
 {
    printk( KERN_INFO "---imx23_dwams_wlan_on_off:%d\n", enable );
-   if(enable)
-       gpio_set_value(WLAN_EN, 1);
-   else
-       gpio_set_value(WLAN_EN, 0);
+   gpio_set_value(WLAN_EN, (int)enable);
 }
 static void imx23_dwams_wlan_init(void)
 {
@@ -77,9 +82,11 @@ static void imx23_dwams_wlan_init(void)
    gpio_request(WLAN_EN, "wlan_en");
    gpio_direction_output(WLAN_EN, 0);
 
+#ifndef BP_HW_30
    /* WLAN MUX enable */
    gpio_request(WLAN_MUX_EN, "wlan_mux_en");
    gpio_direction_output(WLAN_MUX_EN, 1);
+#endif
 
    /* WLAN clk enable */
    gpio_request(WLAN_CLK_EN, "wlan_clk_en");
@@ -99,13 +106,24 @@ static void imx23_dwams_wlan_plat_data_set(void)
 {
    int ret;
    
+/*
+	void (*set_power)(bool enable);
+	int irq;
+	bool use_eeprom;
+	int board_ref_clock;
+	int board_tcxo_clock;
+	unsigned long platform_quirks;
+	bool pwr_in_suspend;
+*/
    printk( KERN_INFO "---imx23_dwams_wlan_plat_data_set\n" );
    wl12xx.board_ref_clock = WL12XX_REFCLOCK_26;
    wl12xx.irq = gpio_to_irq(WLAN_IRQ);
-// wl12xx.board_tcxo_clock = WL12XX_TCXOCLOCK_26;
+   wl12xx.board_tcxo_clock = WL12XX_TCXOCLOCK_26;
 // wl12xx.platform_quirks = WL12XX_PLATFORM_QUIRK_EDGE_IRQ;
-// wl12xx.set_power = imx23_dwams_wlan_on_off;
-
+/* Not used with 1271. Do we need this at all ?
+   ...could be also set by using gpio i/f
+*/
+   wl12xx.set_power = imx23_dwams_wlan_on_off;
    ret = wl12xx_set_platform_data(&wl12xx);
    if (ret) {
        pr_err("error setting wl12xx data: %d\n", ret);
@@ -128,7 +146,22 @@ static void imx23_dwams_bt_init(void)
 // gpio_request(VBUS_EN, "Vbus_en");
 // gpio_direction_output(VBUS_EN, 1);
 // gpio_export(VBUS_EN, true);
+   gpio_request(EN_VBUS_CSR, "en_Vbus_csr");
+   gpio_direction_output(EN_VBUS_CSR, 1);
+   gpio_export(EN_VBUS_CSR, true);
 
+   gpio_request(EN_VBUS_IMX, "en_Vbus_imx");
+   gpio_direction_output(EN_VBUS_IMX, 0);
+   gpio_export(EN_VBUS_IMX, true);
+	
+   gpio_request(MX_CSR_1, "mx_csr_1");
+   gpio_direction_input(MX_CSR_1);
+//	gpio_export(MX_CSR_1, true);
+
+   gpio_request(MX_CSR_2, "mx_csr_1");
+   gpio_direction_input(MX_CSR_2);
+//	gpio_export(MX_CSR_2, true);
+	
 }
 
 static void imx23_dwams_audio_init(void)
@@ -142,8 +175,37 @@ static void imx23_dwams_audio_init(void)
    gpio_request(MIC_GPIO_EN, "saif_mic_en");
    gpio_direction_output(MIC_GPIO_EN, 1);
    gpio_export(MIC_GPIO_EN, true);
-
 }
+
+#ifdef ENABLE_DEBUG_PINS
+void imx23_dwams_debug_pins_init(void)
+{
+	printk( KERN_INFO "---imx23_dwams_debug_pins_init\n" );
+
+	/* TP17 */
+	gpio_request(DEBUG_TP17, "debug_tp17");
+	gpio_direction_output(DEBUG_TP17, 0);
+	gpio_export(DEBUG_TP17, true);
+
+	/* TP20 */
+	gpio_request(DEBUG_TP20, "debug_tp20");
+	gpio_direction_output(DEBUG_TP20, 0);
+	gpio_export(DEBUG_TP20, true);
+}
+ 
+void dwams_dpin17_set(bool set)
+{
+	gpio_set_value(DEBUG_TP17, (int)set);
+}
+EXPORT_SYMBOL(dwams_dpin17_set);
+
+void dwams_dpin20_set(bool set)
+{
+	gpio_set_value(DEBUG_TP20, (int)set);
+}
+EXPORT_SYMBOL(dwams_dpin20_set);
+#endif
+
 
 void __init imx23_dwams_init(void)
 {
@@ -152,4 +214,7 @@ void __init imx23_dwams_init(void)
    imx23_dwams_wlan_plat_data_set();
    imx23_dwams_bt_init();
    imx23_dwams_audio_init();
+#ifdef ENABLE_DEBUG_PINS
+   imx23_dwams_debug_pins_init();
+#endif
 }
